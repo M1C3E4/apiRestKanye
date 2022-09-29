dd261ff80e0d47e9131d6dd774ad5ba97fbbb2bd
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-09-29 13:02:34 +0100

    added method senGet

--- src/main/java/org/example/ApiKayneRest.java
+++ src/main/java/org/example/ApiKayneRest.java
@@ -1,12 +1,26 @@
 package org.example;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ApiKayneRest {
 
     private final HttpClient httpClient = HttpClient.newBuilder().build();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        ApiKayneRest apiKayneRest = new ApiKayneRest();
+        apiKayneRest.sendGet();
+    }
+    private void sendGet() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://api.kanye.rest"))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot")
+                .build();
 
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
