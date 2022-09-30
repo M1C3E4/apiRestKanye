a0490de86f1b1266a004a47352e89aaaafa042b9
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-09-30 10:56:24 +0100

    refactor: loop and added constant

--- src/main/java/org/example/ApiKayneRest.java
+++ src/main/java/org/example/ApiKayneRest.java
@@ -10,23 +10,28 @@ public class ApiKayneRest {
 
     private final HttpClient httpClient = HttpClient.newBuilder().build();
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         ApiKayneRest apiKayneRest = new ApiKayneRest();
         Scanner scanner = new Scanner(System.in);
         System.out.println("*Pierwsza perełka mądrości Kanye Westa: ");
         apiKayneRest.sendGet();
-        for(int i = 0; i<101; i++) {
-            System.out.println("*Jeżeli chcesz poznać kolejną perełkę mądrości Kanye Westa wpisz next:");
+        while (true) {
+            System.out.println("*Jeżeli chcesz poznać kolejną perełkę mądrości Kanye Westa wpisz next w przeciwnym razie wpisz end:");
             String next = scanner.next();
             if (next.equals("next")) {
                 apiKayneRest.sendGet();
+            } else {
+                break;
             }
         }
     }
+
+    private final String url = "https://api.kanye.rest";
+
     private void sendGet() throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
-                .uri(URI.create("https://api.kanye.rest"))
+                .uri(URI.create(url))
                 .setHeader("User-Agent", "Java 11 HttpClient Bot")
                 .build();
 
